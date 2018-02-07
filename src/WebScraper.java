f97b338807147dfd712d2b30680bf919ad7efcbe
John Rudolph <jmr2@illinois.edu>
2018-02-07 12:37:56 -0600

    count one word and word count methods

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,43 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+    public static int wordCount(final String text){
+        String[] arraytext = text.split(" ");
+        int wordcount = 0;
+        for (int index = 0; index < arraytext.length; index++)
+            wordcount++;
+        return wordcount;
+    }
+    public static int countOneWord(final String text, final String word){
+        String[] arraytext = text.split(word);
+        return arraytext.length -1;
+    }
+
+//    public static int uniqueWordCount(final String url)
     public static void main(String[] unused) {
-        System.out.println("Ran");
+        System.out.println(wordCount(urlToString("http://erdani.com/tdpl/hamlet.txt")));
+        System.out.println(countOneWord(urlToString("http://erdani.com/tdpl/hamlet.txt"), "Hamlet"));
+
     }
 }
